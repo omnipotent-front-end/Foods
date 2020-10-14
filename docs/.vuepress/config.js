3ceda31abf60c01c341d70ef7b19fbecbe9948ce
刘放 <362512489@qq.com>
2020-10-14 10:04:18 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: "无所不能的前端--健康指南",
+    base: "/Foods/",
+    description: "Hello world",
+    themeConfig: {
+      repo: "omnipotent-front-end/Foods",
+      nav: [{ text: "awesome-url", link: "https://brizer.github.io/urls/zh/" },{ text: "设计模式", link: "https://omnipotent-front-end.github.io/-Design-Patterns-Typescript/#/" }],
+      sidebarDepth: 2,
+      sidebar: {
+        "": [
+          {
+            title: "食物",
+            children: [
+              "/food/beans.md",
+              "/food/egg.md",
+              "/food/juice.md",
+              "/food/meat.md",
+              "/food/nut.md",
+            ]
+          }
+        ]
+      }
+    }
+  };
+  
